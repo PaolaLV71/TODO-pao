4b90203764f2a0f18003885e33ee59ff1323aeb0
PaolaLV71 <paolalaure71@gmail.com>
2025-03-01 00:23:27 -0400

    funcionalidad de agregar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { JSX, useState } from 'react';
 import { Todos } from './components/Todos';
-import { Todo, TodoId } from './components/types';
+import { Todo, TodoId, TodoTitle } from './components/types';
+import { AddTodo } from './components/addTodo';
 
-const mockTodos = [
+const mockTodos: Todo[] = [
   {
     id: '1',
     title: 'Gato',
@@ -26,9 +27,51 @@ const App = (): JSX.Element => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   };
+  const onCompletedTodo = ({ id }: TodoId): void => {
+    const newTodos: Todo[] = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          id: todo.id,
+          title: todo.title,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+  const onCreatedTodo = ({ title }: TodoTitle) => {
+    console.log(title);
+
+    const nuevoTodo: Todo = {
+      id: (todos.length + 1).toString(),
+      title: title,
+      completed: false,
+    };
+
+    setTodos([...todos, nuevoTodo]);
+  };
+
   return (
-    <div className="todoapp">
-      <Todos onRemoveTodo={handleRemove} todos={todos} />
+    <div
+      className="todoapp"
+      style={{
+        fontSize: 50,
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        <AddTodo onCreatedTodo={onCreatedTodo} />
+        <Todos
+          onRemoveTodo={handleRemove}
+          todos={todos}
+          onCompletedTodo={onCompletedTodo}
+        />
+      </div>
     </div>
   );
 };
